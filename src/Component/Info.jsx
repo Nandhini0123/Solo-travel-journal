548245cd8ad3e489e68cbd04de236f2f5b4df0c8
Nandhini0123 <112197061+Nandhini0123@users.noreply.github.com>
2023-12-12 07:08:31 -0600

    update google maps and image link

--- src/Component/Info.jsx
+++ src/Component/Info.jsx
@@ -3,13 +3,14 @@ export default function Info(props) {
     return (
         <div className="location-details">
          
-          <img className="location-image" src={`../public/Images/${props.imageUrl}`} alt={`picture of the ${props.title}`}/>
+          {/* <img className="location-image" src={`../public/Images/${props.imageUrl}`} alt={`picture of the ${props.title}`}/> */}
+          <img className="location-image" src={`./public/Images/${props.imageUrl}`} alt={`picture of the ${props.title}`}/>
           
           <div className="location-info">
             <div className="location-name">
             <i class="fa-solid fa-location-dot"></i>
             <p className="country">{props.location}</p>
-            <a href="{props.googleMapsUrl}" target="blank">view on google map</a>
+            <a href={props.googleMapsUrl} target="blank">view on google map</a>
             </div>
             
             <h1 className="location">{props.title}</h1>
--- src/data.jsx
+++ src/data.jsx
@@ -1,28 +1,19 @@
 export default [
     {
-        title: "Holland",
-        location: "Michigan, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "13 May, 2022",
-        endDate: "14 May, 2022",
-        description: "Holland is a city on the shore of Lake Macatawa, in Michigan. The much-photographed Big Red Lighthouse stands by the channel that connects this lake to Lake Michigan. May’s Tulip Time Festival, recalling Holland’s Dutch roots, sees tulips blooming all over the city.",
-        imageUrl: "Holland.jpg"
-    },
-    
-    {
         title: "Key West",
         location: "Florida, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/wmLjNDJB5zMT434L6",
         startDate: "12 April, 2018",
         endDate: "15 April, 2018",
         description: "Key West, a U.S. island city, is part of the Florida Keys archipelago. It's also Florida's southernmost point, lying roughly 90 miles north of Cuba. Famed for its pastel-hued, conch-style houses and the coral reefs",
         imageUrl: "/Keywest.jpg"
     },
+    
 
     {
         title: "Niagara Falls",
         location: "NewYork, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/Gzktx3PL47iakpbo8",
         startDate: "12 August, 2019",
         endDate: "14 August, 2018",
         description: "Niagara Falls is a city on the Niagara River, in New York State. It’s known for the vast Niagara Falls, which straddle the Canadian border. In Niagara Falls State Park, the Observation Tower, at Prospect Point, juts out over Niagara Gorge for a view of all 3 waterfalls.",
@@ -31,7 +22,7 @@ export default [
     {
         title: "Chicago",
         location: "Illinois, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/39QJBS3q52KawBN77",
         startDate: "1 November, 2017",
         endDate: "3 November, 2017",
         description: "Chicago, on Lake Michigan in Illinois, is among the largest cities in the U.S. Famed for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works.",
@@ -40,7 +31,7 @@ export default [
     {
         title: "Washington Island",
         location: "Wisconsin, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/miC9yxgmtP4aGuA78",
         startDate: "18 September, 2021",
         endDate: "20 September, 2021",
         description: "Washington Island is your destination for adventure in Door County. It all starts with a 5-mile ferry ride across the once-treacherous waters of Death’s Door, where Green Bay and Lake Michigan meet. Once ashore, embark on a tour of one-of-a-kind shopping, eating, music and art, remote parks, island festivals, and outdoor fun hosted by the 600 residents who call the island home.",
@@ -50,12 +41,22 @@ export default [
     {
         title: "Door County",
         location: "Wisconsin, USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://maps.app.goo.gl/Hj4J4u6UfjxU9CLL6",
         startDate: "18 September, 2021",
         endDate: "20 September, 2021",
         description: "Known for its picturesque coastal towns, specialty shops, fish boils, cherry and apple orchards and scenic beauty, Door County, Wisconsin is often referred to as the “Cape Cod of the Midwest.” The best time to see fall colors is late-September to mid-October, but spectacular color can be found in the area all autumn long.",
         imageUrl: "Door-county.jpg"
     },
+    {
+        title: "Holland",
+        location: "Michigan, USA",
+        googleMapsUrl: "https://maps.app.goo.gl/h2y4VT8zBpUYqnD4A",
+        startDate: "13 May, 2022",
+        endDate: "14 May, 2022",
+        description: "Holland is a city on the shore of Lake Macatawa, in Michigan. The much-photographed Big Red Lighthouse stands by the channel that connects this lake to Lake Michigan. May’s Tulip Time Festival, recalling Holland’s Dutch roots, sees tulips blooming all over the city.",
+        imageUrl: "Holland.jpg"
+    },
+    
 
     {
         title: "Las Vegas",
@@ -70,12 +71,38 @@ export default [
     {
         title: "Grand Canyon",
         location: "Arizona, USA",
-        googleMapsUrl: "https://maps.app.goo.gl/y7CQMHuCyShzwP9z6",
+        googleMapsUrl: "https://maps.app.goo.gl/NyrUjavwzpE11K6J7",
         startDate: "24 April, 2023",
         endDate: "27 April, 2023",
         description: "The Grand Canyon is a natural formation distinguished by layered bands of red rock, revealing millions of years of geological history in cross-section. Vast in scale, the canyon averages 10 miles across and a mile deep along its 277-mile length. Much of the area is a national park, with Colorado River white-water rapids and sweeping vistas.  ",
         imageUrl: "GrandCanyon.jpg"
     },
+    // {
+    //     title: "Homestead",
+    //     location: "Florida, USA",
+    //     googleMapsUrl: "https://maps.app.goo.gl/g8UR5oh8855cWFnV8",
+    //     startDate: "11 April, 2018",
+    //     endDate: "12 April, 2018",
+    //     description: "Holland is a city on the shore of Lake Macatawa, in Michigan. The much-photographed Big Red Lighthouse stands by the channel that connects this lake to Lake Michigan. May’s Tulip Time Festival, recalling Holland’s Dutch roots, sees tulips blooming all over the city.",
+    //     imageUrl: "Holland.jpg"
+    // },
+    // {
+    //     title: "Miami",
+    //     location: "Florida, USA",
+    //     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    //     startDate: "10 April, 2018",
+    //     endDate: "11 April, 2018",
+    //     description: "Holland is a city on the shore of Lake Macatawa, in Michigan. The much-photographed Big Red Lighthouse stands by the channel that connects this lake to Lake Michigan. May’s Tulip Time Festival, recalling Holland’s Dutch roots, sees tulips blooming all over the city.",
+    //     imageUrl: "Holland.jpg"},
+    // {
+    //     title: "Orlando",
+    //     location: "Florida, USA",
+    //     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    //     startDate: "9 April, 2018",
+    //     endDate: "10 April, 2018",
+    //     description: "Holland is a city on the shore of Lake Macatawa, in Michigan. The much-photographed Big Red Lighthouse stands by the channel that connects this lake to Lake Michigan. May’s Tulip Time Festival, recalling Holland’s Dutch roots, sees tulips blooming all over the city.",
+    //     imageUrl: "Holland.jpg"
+    // }
 
 
 
